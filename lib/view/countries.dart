8d5afa9a97a61b87e9af3302271bc761f7530f5e
MITU <mh7735791@gmail.com>
2026-08-19 21:24:44 +0600

    countries

--- lib/view/countries.dart
+++ lib/view/countries.dart
@@ -9,30 +9,44 @@ class Countries extends StatefulWidget {
 }
 
 class _CountriesState extends State<Countries> {
-List cl = [];
+  List cl = [];
 
-  getData()async{
-   var a = await CountriesService().getCountries();
-   cl.clear();
-   cl.addAll(a);
-   setState(() {});
+  getData() async {
+    var a = await CountriesService().getCountries();
+    cl.clear();
+    cl.addAll(a);
+    setState(() {});
   }
+
   @override
   void initState() {
     getData();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Countries - ${cl.length}"), centerTitle: true,actions: [
-IconButton(onPressed: (){
-  getData();
-}, icon: Icon(Icons.refresh))      ],),
+      appBar: AppBar(
+        title: Text("Countries - ${cl.length}"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              getData();
+            },
+            icon: Icon(Icons.refresh),
+          ),
+        ],
+      ),
       body: ListView.builder(
         itemCount: cl.length,
-        itemBuilder: (c, i) =>
-            ListTile(title: Text("${cl[i]["name"]}"), subtitle: Text("${cl[i]["admissionDateUnitedNations"]}")),
+        // itemCount: cl.length,
+        itemBuilder: (c, i) => ListTile(
+          title: Text("${cl[i]["name"]}"),
+         // title: Text("${cl[i]["name"]}"),
+          subtitle: Text("${cl[i]["admissionDateUnitedNations"]}"),
+        ),
       ),
     );
   }
